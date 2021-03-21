ba56876ce0d34cf8405881118171f6cae59d3221
Timae1301 <timscheffler@t-online.de>
2021-03-21 19:42:47 +0100

    Fahrplan nur von einer Buslinie

--- src/main/java/de/hsw/busplaner/services/FahrplanzuordnungService.java
+++ src/main/java/de/hsw/busplaner/services/FahrplanzuordnungService.java
@@ -3,7 +3,9 @@ package de.hsw.busplaner.services;
 import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import de.hsw.busplaner.beans.Fahrplan;
 import de.hsw.busplaner.beans.Fahrplanzuordnung;
@@ -34,10 +36,26 @@ public class FahrplanzuordnungService extends BasicService<Fahrplanzuordnung, Lo
     public Long postFahrplanzuordnung(FahrplanzuordnungInputDTO fahrplanzuordnungInputDTO) {
         Fahrtstrecke fahrtstrecke = fahrtstreckeService
                 .getFahrtstreckeZuId(fahrplanzuordnungInputDTO.getFahrtstreckeId());
+        pruefeGueltigkeitDerFahrtstreckeFuerFahrplan(fahrplanzuordnungInputDTO, fahrtstrecke.getBuslinieId().getId());
         Fahrplan fahrplan = fahrplanService.getFahrplanZuId(fahrplanzuordnungInputDTO.getFahrplanId());
         Fahrplanzuordnung fahrplanzuordnung = new Fahrplanzuordnung(fahrplanzuordnungInputDTO, fahrtstrecke, fahrplan);
         save(fahrplanzuordnung);
         return fahrplanzuordnung.getId();
+    }
+
+    private void pruefeGueltigkeitDerFahrtstreckeFuerFahrplan(FahrplanzuordnungInputDTO fahrplanzuordnungInputDTO,
+            Long buslinieId) {
+        ArrayList<Fahrplanzuordnung> fahrplanzuordnungen = getAlleFahrplanzuordnungenZuFahrplanId(
+                fahrplanService.getFahrplanZuId(fahrplanzuordnungInputDTO.getFahrplanId()));
+        if (fahrplanzuordnungen.isEmpty()) {
+            return;
+        }
+        Long buslinieIdFahrplan = fahrplanzuordnungen.get(0).getFahrtstreckeid().getBuslinieId().getId();
+        if (buslinieIdFahrplan.equals(buslinieId)) {
+            return;
+        }
+        throw new ResponseStatusException(HttpStatus.UNPROCESSABLE_ENTITY,
+                String.format("Die Fahrtstrecke muss von der Buslinie %s sein", buslinieIdFahrplan));
     }
 
     public ArrayList<FahrplanzuordnungOutputDTO> getAlleFahrplanzuordnungen() {
